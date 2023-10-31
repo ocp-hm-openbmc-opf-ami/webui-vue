400cda2be3369acbd232284d34a198bc72dc8a14
Lakshmanakumar Velusamy <lakshmanakumarv@ami.com>
2023-10-31 08:15:41 -0400

    OT-2119 Adding router configuration

--- src/env/router/ami.js
+++ src/env/router/ami.js
@@ -37,6 +37,7 @@ import Dumps from '@/views/Logs/Dumps';
 import ACD from '@/views/HostSystemDiagnostics/ACD';
 import Asd from '@/views/HostSystemDiagnostics/Asd';
 import BackupAndRestore from '@/views/Operations/BackupAndRestore';
+import systemInventory from '@/views/SystemInventory/systemInventory';
 
 const roles = {
   administrator: 'Administrator',
@@ -345,6 +346,14 @@ const routes = [
             roles.operator,
             roles.readonly,
           ],
+        },
+      },
+      {
+        path: '/system-inventory',
+        name: 'system-inventory',
+        component: systemInventory,
+        meta: {
+          title: i18n.t('appPageTitle.system-inventory'),
         },
       },
     ],
