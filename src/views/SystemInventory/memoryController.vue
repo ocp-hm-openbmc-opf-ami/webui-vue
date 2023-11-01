fae2787e49095f6b27f6a0e6f7ac5b75839e7652
Lakshmanakumar Velusamy <lakshmanakumarv@ami.com>
2023-11-01 09:33:25 -0400

    System inventory ot 2119

---
+++ src/views/SystemInventory/memoryController.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <b-table
+      responsive="md"
+      hover
+      sticky-header
+      :fields="MemoryControllerFields"
+      head-variant="light"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      MemoryControllerFields: [
+        {
+          key: 'Id',
+          label: this.$t('pageSystemInventory.MemoryController.Id'),
+        },
+        {
+          key: 'Name',
+          label: this.$t('pageSystemInventory.MemoryController.Name'),
+        },
+        {
+          key: 'CapacityMiB',
+          label: this.$t('pageSystemInventory.MemoryController.CapacityMiB'),
+        },
+        {
+          key: 'Manufacturer',
+          label: this.$t('pageSystemInventory.MemoryController.Manufacturer'),
+        },
+        {
+          key: 'SerialNmuber',
+          label: this.$t('pageSystemInventory.MemoryController.SerialNmuber'),
+        },
+        {
+          key: 'PartNmuber',
+          label: this.$t('pageSystemInventory.MemoryController.PartNmuber'),
+        },
+        {
+          key: 'State',
+          label: this.$t('pageSystemInventory.MemoryController.State'),
+        },
+        {
+          key: 'OperatingSpeedMhz',
+          label: this.$t(
+            'pageSystemInventory.MemoryController.OperatingSpeedMhz'
+          ),
+        },
+        {
+          key: 'MemoryType',
+          label: this.$t('pageSystemInventory.MemoryController.MemoryType'),
+        },
+        {
+          key: 'Description',
+          label: this.$t('pageSystemInventory.MemoryController.Description'),
+        },
+        {
+          key: 'AllowedSpeedsMHz',
+          label: this.$t(
+            'pageSystemInventory.MemoryController.AllowedSpeedsMHz'
+          ),
+        },
+        {
+          key: 'ServiceLabel',
+          label: this.$t('pageSystemInventory.MemoryController.ServiceLabel'),
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/views/SystemInventory/storageController.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-table
+      responsive="md"
+      hover
+      sticky-header
+      :fields="StorageControllerFields"
+      head-variant="light"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      StorageControllerFields: [
+        {
+          key: 'MemberId',
+          label: this.$t('pageSystemInventory.StorageController.MemberId'),
+        },
+        {
+          key: 'Name',
+          label: this.$t('pageSystemInventory.StorageController.Name'),
+        },
+        {
+          key: 'SerialNmuber',
+          label: this.$t('pageSystemInventory.StorageController.SerialNmuber'),
+        },
+        {
+          key: 'Model',
+          label: this.$t('pageSystemInventory.StorageController.Model'),
+        },
+        {
+          key: 'FirmwareVersion',
+          label: this.$t(
+            'pageSystemInventory.StorageController.FirmwareVersion'
+          ),
+        },
+        {
+          key: 'SpeedGbs',
+          label: this.$t('pageSystemInventory.StorageController.SpeedGbs'),
+        },
+        {
+          key: 'State',
+          label: this.$t('pageSystemInventory.StorageController.State'),
+        },
+      ],
+    };
+  },
+};
+</script>
