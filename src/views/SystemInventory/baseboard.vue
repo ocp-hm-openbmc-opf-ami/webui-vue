fe7120dde1b4dbff0069369251a3482f65ebe07c
Lakshmanakumar Velusamy <lakshmanakumarv@ami.com>
2023-11-01 09:33:25 -0400

    System inventory ot 2119

---
+++ src/views/SystemInventory/baseboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <b-table
+      responsive="md"
+      hover
+      sticky-header
+      :fields="BaseboardFields"
+      head-variant="light"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      BaseboardFields: [
+        {
+          key: 'Name',
+          label: this.$t('pageSystemInventory.BaseBoard.Name'),
+        },
+        {
+          key: 'Description',
+          label: this.$t('pageSystemInventory.BaseBoard.Description'),
+        },
+        {
+          key: 'FrmwareVersion',
+          label: this.$t('pageSystemInventory.BaseBoard.FrmwareVersion'),
+        },
+        {
+          key: 'Model',
+          label: this.$t('pageSystemInventory.BaseBoard.Model'),
+        },
+        {
+          key: 'State',
+          label: this.$t('pageSystemInventory.BaseBoard.State'),
+        },
+        {
+          key: 'PowerState',
+          label: this.$t('pageSystemInventory.BaseBoard.PowerState'),
+        },
+      ],
+    };
+  },
+};
+</script>
