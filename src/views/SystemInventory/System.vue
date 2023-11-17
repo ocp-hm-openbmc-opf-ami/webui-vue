ce16deefbf9d3f89cd324f09998515d8290fafea
Thrinadh <thrinadhkumarv@ami.com>
2023-11-17 10:08:15 -0500

    OT-2119 fetched data to store

--- src/views/SystemInventory/System.vue
+++ src/views/SystemInventory/System.vue
@@ -6,6 +6,7 @@
       sticky-header
       :fields="systemFields"
       head-variant="light"
+      :items="systemTabItems()"
     />
   </div>
 </template>
@@ -18,53 +19,105 @@ export default {
         {
           key: 'Name',
           label: this.$t('pageSystemInventory.system.Name'),
+          class: 'text-center',
         },
         {
           key: 'Description',
           label: this.$t('pageSystemInventory.system.Description'),
+          class: 'text-center',
         },
         {
           key: 'IndicatorLED',
           label: this.$t('pageSystemInventory.system.IndicatorLED'),
+          class: 'text-center',
         },
         {
           key: 'Manufacture',
           label: this.$t('pageSystemInventory.system.Manufacture'),
+          class: 'text-center',
         },
         {
           key: 'PowerState',
           label: this.$t('pageSystemInventory.system.PowerState'),
+          class: 'text-center',
         },
         {
           key: 'SerialNmuber',
           label: this.$t('pageSystemInventory.system.SerialNmuber'),
+          class: 'text-center',
         },
         {
           key: 'PartNmuber',
           label: this.$t('pageSystemInventory.system.PartNmuber'),
+          class: 'text-center',
         },
         {
           key: 'SystemType',
           label: this.$t('pageSystemInventory.system.SystemType'),
+          class: 'text-center',
         },
         {
           key: 'AssetTag',
           label: this.$t('pageSystemInventory.system.AssetTag'),
+          class: 'text-center',
         },
         {
           key: 'BiosVersion',
           label: this.$t('pageSystemInventory.system.BiosVersion'),
+          class: 'text-center',
         },
         {
           key: 'UUID',
           label: this.$t('pageSystemInventory.system.UUID'),
+          class: 'text-center',
         },
         {
           key: 'state',
           label: this.$t('pageSystemInventory.system.state'),
+          class: 'text-center',
         },
       ],
     };
   },
+  computed: {
+    SystemInfo() {
+      return this.$store.getters['SystemInventoryStore/getSystemsInfo'];
+    },
+  },
+  created() {
+    this.$store.dispatch('SystemInventoryStore/getSystemsInfo');
+    console.log(
+      this.$store.getters['SystemInventoryStore/systems'],
+      'systemTabItems'
+    );
+  },
+  methods: {
+    systemTabItems() {
+      console.log(
+        this.$store.getters['SystemInventoryStore/systems'],
+        'systemTabItems'
+      );
+    },
+  },
+  // systemTabItems() {
+  //   // transform system tab data to table data
+  //   return this.SystemInfo;
+  //   // return {
+  //   //   Name: data[index].Name,
+  //   //   Description: data[index].Description,
+  //   //   Model: data[index].Model,
+  //   //   IndicatorLED: data[index].IndicatorLED,
+  //   //   Manufacture: data[index].Manufacture,
+  //   //   PowerState: data[index].PowerState,
+  //   //   SerialNmuber: data[index].SerialNmuber,
+  //   //   PartNmuber: data[index].PartNmuber,
+  //   //   SystemType: data[index].SystemType,
+  //   //   AssetTag: data[index].AssetTag,
+  //   //   BiosVersion: data[index].BiosVersion,
+  //   //   UUID: data[index].UUID,
+  //   //   state: data[index].state,
+  //   // };
+  //   // });
+  // },
 };
 </script>
