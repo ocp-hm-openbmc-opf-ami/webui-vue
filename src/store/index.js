c6942772acf167238b8cbdb4e0b507ca7fe406f2
Thrinadh <thrinadhkumarv@ami.com>
2023-11-17 10:08:15 -0500

    OT-2119 fetched data to store

--- src/store/index.js
+++ src/store/index.js
@@ -45,6 +45,7 @@ import AutonomousCrashDumpStore from './modules/HostSystemDiagnostics/AcdStore';
 import AsdStore from './modules/HostSystemDiagnostics/AsdStore';
 import OnDemand from './modules/Settings/OnDemand';
 import RaidStore from './modules/Raid/RaidStore';
+import BackupAndRestore from './modules/Operations/BackupAndRestore';
 import SystemInfoStore from './modules/SystemInventory/SystemInventoryStore';
 
 Vue.use(Vuex);
@@ -97,6 +98,7 @@ export default new Vuex.Store({
     asd: AsdStore,
     ondemand: OnDemand,
     raid: RaidStore,
+    backupAndRestore: BackupAndRestore,
     SystemStore: SystemInfoStore,
   },
   plugins: [WebSocketPlugin],
