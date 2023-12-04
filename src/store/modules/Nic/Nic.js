99f530f79cbb672ebb88301cd2a84a9b81347ccc
Aravinth S <aravinths@ami.com>
2023-12-04 01:26:45 +0000

    WEBUI - Updating NIC OEM properties

--- src/store/modules/Nic/Nic.js
+++
@@ -1,6 +0,0 @@
-import api from '@/store/api';
-const NicStore = {
-  namespaced: true,
-};
-console.log('api:', api);
-export default NicStore;
--- src/store/modules/NvmeInformation/NvmeInformationStore.js
+++
@@ -1,7 +0,0 @@
-import api from '@/store/api';
-const NetworkStore = {
-  namespaced: true,
-};
-
-console.log('api:', api);
-export default NetworkStore;
