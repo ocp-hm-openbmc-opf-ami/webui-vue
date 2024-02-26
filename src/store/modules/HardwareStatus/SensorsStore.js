aa9d2af14abc4751d5ea9840ae9bbfb9ef2b80ff
Lakshmanakumar Velusamy <lakshmanakumarv@ami.com>
2024-02-26 19:50:47 +0530

    OT-2822 Webui support for graph sensor
    Signed-off-by: Lakshmanakumar Velusamy <lakshmanakumarv@ami.com>

--- src/store/modules/HardwareStatus/SensorsStore.js
+++ src/store/modules/HardwareStatus/SensorsStore.js
@@ -5,13 +5,18 @@ const SensorsStore = {
   namespaced: true,
   state: {
     sensors: [],
+    graphsensors: {},
   },
   getters: {
     sensors: (state) => state.sensors,
+    GraphSensors: (state) => state.graphsensors,
   },
   mutations: {
     setSensors: (state, sensors) => {
       state.sensors = uniqBy([...sensors, ...state.sensors], 'name');
+    },
+    SetGraphSensors: (state, sensors) => {
+      state.graphsensors = sensors;
     },
   },
   actions: {
@@ -51,6 +56,7 @@ const SensorsStore = {
         responses.forEach((response) => {
           if (response.data) {
             sensorData.push({
+              id: response.data['@odata.id'],
               name: response.data.Name,
               status: response.data.Status?.Health,
               currentValue: response.data.Reading,
@@ -115,6 +121,134 @@ const SensorsStore = {
             };
           });
           commit('setSensors', sensorData);
+        })
+        .catch((error) => console.log(error));
+    },
+    async getTimeInterval({ commit }, val) {
+      return await api
+        .get(val.id)
+        .then((response) => {
+          var sensorGraphData = {};
+          if (response.data) {
+            if (val.value == 10) {
+              sensorGraphData = {
+                id: response.data['@odata.id'],
+                name: response.data.Name,
+                status: response.data.Status?.Health,
+                currentValue: response.data.Reading,
+                lowerCaution: response.data.Thresholds?.LowerCaution?.Reading,
+                upperCaution: response.data.Thresholds?.UpperCaution?.Reading,
+                lowerCritical: response.data.Thresholds?.LowerCritical?.Reading,
+                upperCritical: response.data.Thresholds?.UpperCritical?.Reading,
+                units: response.data.ReadingUnits,
+                History: [
+                  {
+                    ReadingCelsius: 10.5,
+                    Time: 1708974060,
+                  },
+                  {
+                    ReadingCelsius: 5.5,
+                    Time: 1708974629,
+                  },
+                  {
+                    ReadingCelsius: 15.5,
+                    Time: 1708975247,
+                  },
+                  {
+                    ReadingCelsius: 20.5,
+                    Time: 1708975857,
+                  },
+                  {
+                    ReadingCelsius: 13.0,
+                    Time: 1708976469,
+                  },
+                  {
+                    ReadingCelsius: 23.0,
+                    Time: 1708977022,
+                  },
+                ],
+              };
+            }
+            if (val.value == 20) {
+              sensorGraphData = {
+                id: response.data['@odata.id'],
+                name: response.data.Name,
+                status: response.data.Status?.Health,
+                currentValue: response.data.Reading,
+                lowerCaution: response.data.Thresholds?.LowerCaution?.Reading,
+                upperCaution: response.data.Thresholds?.UpperCaution?.Reading,
+                lowerCritical: response.data.Thresholds?.LowerCritical?.Reading,
+                upperCritical: response.data.Thresholds?.UpperCritical?.Reading,
+                units: response.data.ReadingUnits,
+                History: [
+                  {
+                    ReadingCelsius: 0.5,
+                    Time: 1708974060,
+                  },
+                  {
+                    ReadingCelsius: 5.5,
+                    Time: 1708975247,
+                  },
+                  {
+                    ReadingCelsius: 7.5,
+                    Time: 1708976434,
+                  },
+                  {
+                    ReadingCelsius: 8.5,
+                    Time: 1708977621,
+                  },
+                  {
+                    ReadingCelsius: 9.5,
+                    Time: 1708978808,
+                  },
+                  {
+                    ReadingCelsius: 15.5,
+                    Time: 1708979995,
+                  },
+                ],
+              };
+            }
+            if (val.value == 30) {
+              sensorGraphData = {
+                id: response.data['@odata.id'],
+                name: response.data.Name,
+                status: response.data.Status?.Health,
+                currentValue: response.data.Reading,
+                lowerCaution: response.data.Thresholds?.LowerCaution?.Reading,
+                upperCaution: response.data.Thresholds?.UpperCaution?.Reading,
+                lowerCritical: response.data.Thresholds?.LowerCritical?.Reading,
+                upperCritical: response.data.Thresholds?.UpperCritical?.Reading,
+                units: response.data.ReadingUnits,
+                History: [
+                  {
+                    ReadingCelsius: 1.5,
+                    Time: 1708974060,
+                  },
+                  {
+                    ReadingCelsius: 2.5,
+                    Time: 1708975860,
+                  },
+                  {
+                    ReadingCelsius: 3.5,
+                    Time: 1708977660,
+                  },
+                  {
+                    ReadingCelsius: 4.5,
+                    Time: 1708979460,
+                  },
+                  {
+                    ReadingCelsius: 5.5,
+                    Time: 1708981260,
+                  },
+                  {
+                    ReadingCelsius: 6.5,
+                    Time: 1708983060,
+                  },
+                ],
+              };
+            }
+          }
+          commit('SetGraphSensors', sensorGraphData);
         })
         .catch((error) => console.log(error));
     },
