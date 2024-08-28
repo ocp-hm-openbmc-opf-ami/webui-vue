a41301d7ae3392bdfcd9aabb6665e6ec75c72f29
Sivaprabu G <sivaprabug@ami.com>
2024-08-28 03:44:51 +0000

    CPU information in System Inventory Web Page

---
+++ src/views/SystemInventory/FPGA.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <b-table
+      responsive="md"
+      hover
+      sticky-header
+      :fields="processorFpgaFields"
+      :items="processorFpgaInfo"
+      head-variant="light"
+    ></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      processorFpgaFields: [
+        {
+          key: 'id',
+          label: this.$t('pageSystemInventory.processorFPGA.id'),
+        },
+        {
+          key: 'name',
+          label: this.$t('pageSystemInventory.processorFPGA.name'),
+        },
+        {
+          key: 'firmwareId',
+          label: this.$t('pageSystemInventory.processorFPGA.firmwareId'),
+        },
+        {
+          key: 'firmwareManufacturer',
+          label: this.$t(
+            'pageSystemInventory.processorFPGA.firmwareManufacturer',
+          ),
+        },
+        {
+          key: 'fpgaType',
+          label: this.$t('pageSystemInventory.processorFPGA.fpgaType'),
+        },
+        {
+          key: 'interfaceType',
+          label: this.$t('pageSystemInventory.processorFPGA.interfaceType'),
+        },
+        {
+          key: 'fpgaModel',
+          label: this.$t('pageSystemInventory.processorFPGA.model'),
+        },
+        {
+          key: 'pcieVirtualFunctions',
+          label: this.$t(
+            'pageSystemInventory.processorFPGA.pcieVirtualFunctions',
+          ),
+        },
+        {
+          key: 'programmableFromHost',
+          label: this.$t(
+            'pageSystemInventory.processorFPGA.programmableFromHost',
+          ),
+        },
+      ],
+    };
+  },
+  computed: {
+    processorFpgaInfo() {
+      return this.$store.getters['SystemStore/fpga'];
+    },
+  },
+};
+</script>
