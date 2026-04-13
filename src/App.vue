a71cfd64800e172858ad20d7d1547936a71bc6e4
Lakshmanakumar Velusamy <lakshmanakumarv@ami.com>
2026-04-13 19:43:24 +0000

    Blocked logout session for F5 button click event.

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,13 @@ import Cookies from 'js-cookie';
 import i18n from '@/i18n';
 export default {
   name: 'App',
+  data() {
+    return {
+      F5detect: false,
+      isFirmwareLoaderActive: false,
+    };
+  },
+  computed: {},
   watch: {
     $route: function (to) {
       document.title =
@@ -20,6 +27,9 @@ export default {
   },
   created() {
     Cookies.set('loginSessionSuccess', 'true');
+    this.$root.$on('loader-start', this.onLoaderStart);
+    this.$root.$on('loader-end', this.onLoaderStop);
+    this.$root.$on('loader-hide', this.onLoaderStop);
     document.title =
       i18n.t('global.ariaLabel.megaracOneTree') + this.$route.meta.title ||
       'Page is missing title';
@@ -29,6 +39,8 @@ export default {
       this.$store.dispatch('global/getManagerinstance');
     }
     window.addEventListener('unload', this.handleRefresh);
+    document.addEventListener('keyup', this.handleKeyUp);
+    document.addEventListener('keydown', this.handleKeyDown);
     setTimeout(() => {
       if (
         Cookies.get('XSRF-TOKEN') &&
@@ -45,22 +57,61 @@ export default {
     }, 2000); // wait to read the XSRF-TOKEN
   },
   beforeDestroy() {
+    this.$root.$off('loader-start', this.onLoaderStart);
+    this.$root.$off('loader-end', this.onLoaderStop);
+    this.$root.$off('loader-hide', this.onLoaderStop);
     window.removeEventListener('beforeunload', this.handleRefresh);
     window.removeEventListener('unload', this.handleRefresh);
+    document.removeEventListener('keyup', this.handleKeyUp);
+    document.removeEventListener('keydown', this.handleKeyDown);
   },
   methods: {
+    onLoaderStart() {
+      if (this.$route.path === '/operations/firmware') {
+        this.isFirmwareLoaderActive = true;
+      }
+    },
+    onLoaderStop() {
+      this.isFirmwareLoaderActive = false;
+    },
+    // Handle keyup events - reset F5detect flag
+    handleKeyUp() {
+      this.F5detect = false;
+    },
+    // Handle keydown events - intercept F5 key
+    handleKeyDown(e) {
+      if ((e.which || e.keyCode) === 116 && this.F5detect === false) {
+        /* Disabled F5 button and trigger customized refresh.*/
+        this.F5detect = true;
+        e.preventDefault();
+        this.handleRefresh(e);
+      }
+    },
     // Perform logout or any other actions needed before unload
     // This function will be called before the page is unloaded or refreshed
     handleRefresh(event) {
+      if (
+        this.$route.path === '/operations/firmware' &&
+        this.isFirmwareLoaderActive &&
+        this.F5detect === true
+      ) {
+        return;
+      }
       if (
         window.name != 'kvmConsoleWindow' &&
         window.name != 'kvm1ConsoleWindow' &&
         window.location.href.indexOf('serial-over-lan-console') == -1 &&
         window.location.href.indexOf('/redfish/v1') == -1
       ) {
-        if (event.srcElement.URL.indexOf('login') == -1) {
+        const eventUrl =
+          (event && event.srcElement && event.srcElement.URL) ||
+          window.location.href;
+        if (eventUrl.indexOf('login') == -1 && this.F5detect === false) {
           console.log('event:', event);
           store.dispatch('authentication/logout');
+        } else {
+          this.F5detect = false;
+          this.$root.$emit('refresh-application');
         }
       }
     },
