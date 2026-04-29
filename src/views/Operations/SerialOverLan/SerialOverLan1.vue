6b8782620b04691b2e58764261546532d585380a
Sivaprabu G <sivaprabug@ami.com>
2026-04-29 11:33:11 +0530

    Serial Over Console ( SOL ) - Dual Node Support

---
+++ src/views/Operations/SerialOverLan/SerialOverLan1.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-container fluid="xl">
+    <page-title class="mb-4" :description="$t('pageSerialOverLan.subTitle')" />
+
+    <page-section class="mb-0">
+      <serial-over-lan-console1 :is-full-window="false" />
+    </page-section>
+  </b-container>
+</template>
+
+<script>
+import PageTitle from '@/components/Global/PageTitle';
+import PageSection from '@/components/Global/PageSection';
+import SerialOverLanConsole1 from './SerialOverLanConsole1';
+
+import LoadingBarMixin from '@/components/Mixins/LoadingBarMixin';
+
+export default {
+  name: 'SerialOverLan1',
+  components: {
+    PageSection,
+    PageTitle,
+    SerialOverLanConsole1,
+  },
+  mixins: [LoadingBarMixin],
+  created() {
+    this.startLoader();
+    this.$store
+      .dispatch('global/getSystemInfo1')
+      .finally(() => this.endLoader());
+  },
+};
+</script>
